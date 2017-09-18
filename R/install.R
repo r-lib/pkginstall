b78cc40b79ade488eaa6f8aac9d723f56e6778be
Jim Hester <james.f.hester@gmail.com>
2017-09-18 13:15:18 -0400

    Fix installation of installing a source tarball
    We simply extract the tarball to a directory, then run `install_source`
    again.

--- R/install.R
+++ R/install.R
@@ -143,11 +143,14 @@ verify_binary <- function(filename) {
 install_source <- function(path, lib = .libPaths()[[1L]],
                                lock = getOption("install.lock", TRUE), quiet = TRUE, ...) {
 
-  if (file.info(path)$isdir) {
-    pkg_name <- desc::desc_get("Package", path)
-  } else {
+  is_tarball <- !file.info(path)$isdir
+  if (identical(is_tarball, TRUE)) {
     pkg_name <- get_archive_pkg_name(archive(path))
+    tmp_path <- tempfile()
+    archive_extract(path, tmp_path)
+    return(Recall(file.path(tmp_path, pkg_name), lib, lock, quiet, ...))
   }
+  pkg_name <- desc::desc_get("Package", path)
 
   lib_cache <- library_cache(lib, pkg_name, lock)
 

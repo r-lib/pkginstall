35011a34ecf8375010a9422988e51e25b4b66a77
Jim Hester <james.f.hester@gmail.com>
2017-11-10 15:31:27 -0500

    Simple green field test for install_package and install_packages

---
+++ tests/testthat/test-install.R
@@ -0,0 +1,45 @@
+describe("install_package", {
+  it("works with source packages", {
+
+  pkg <- "foo_0.0.0.9000.tar.gz"
+  expect_error_free(pkgbuild::build("foo", quiet = TRUE))
+
+  libpath <- create_temp_dir()
+
+  on.exit({
+    detach("package:foo", character.only = TRUE, unload = TRUE)
+    remove.packages("foo", lib = libpath)
+    unlink(libpath, recursive = TRUE)
+    unlink(pkg)
+  })
+
+  expect_error_free(
+    install_package("foo_0.0.0.9000.tar.gz", lib = libpath))
+
+  expect_error_free(
+    library("foo", lib.loc = libpath))
+  })
+})
+
+describe("install_packages", {
+  it("works with source packages", {
+
+  pkg <- "foo_0.0.0.9000.tar.gz"
+  expect_error_free(pkgbuild::build("foo", quiet = TRUE))
+
+  libpath <- create_temp_dir()
+
+  on.exit({
+    detach("package:foo", character.only = TRUE, unload = TRUE)
+    remove.packages("foo", lib = libpath)
+    unlink(libpath, recursive = TRUE)
+    unlink(pkg)
+  })
+
+  expect_error_free(
+    install_packages("foo_0.0.0.9000.tar.gz", lib = libpath))
+
+  expect_error_free(
+    library("foo", lib.loc = libpath))
+  })
+})

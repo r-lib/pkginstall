0ebff1b42fd1577d9b7f48a09396e6691701155b
Gábor Csárdi <csardi.gabor@gmail.com>
2017-12-15 23:55:20 +0000

    archives: internal unzip, untar, tar & zip::zip

--- R/archive.R
+++ R/archive.R
@@ -5,17 +5,17 @@ archive_files <- function(path) {
   path <- normalizePath(path)
 
   if (windows_archive(path)) {
-    return(unzip(path, list = TRUE)$path)
+    return(unzip(path, list = TRUE, unzip = "internal")$Name)
   }
 
-  untar(path, list = TRUE)
+  untar(path, list = TRUE, tar = "internal")
 }
 
 archive_extract <- function(path, dir) {
   if (windows_archive(path)) {
-    unzip(path, exdir = dir)
+    return(unzip(path, exdir = dir, unzip = "internal"))
   }
-  untar(path, exdir = dir)
+  untar(path, exdir = dir, tar = "internal")
 }
 
 windows_archive <- function(path) {
@@ -27,15 +27,16 @@ archive_read <- function(path, file, dir) {
   on.exit(unlink(dir, recursive = TRUE))
   out_path <- file.path(dir, file)
   if (windows_archive(path)) {
-    unzip(path, file, exdir = dir)
+    unzip(path, file, exdir = dir, unzip = "internal")
   } else {
-    untar(path, file, exdir = dir)
+    untar(path, file, exdir = dir, tar = "internal")
   }
 
   readLines(out_path)
 }
 
-#' @importFrom utils zip tar
+#' @importFrom zip zip
+#' @importFrom utils tar
 archive_write_dir <- function(archive, dir, ..., recursive = TRUE, full.names = FALSE) {
   archive <- file.path(normalizePath(dirname(archive)), basename(archive))
   old <- setwd(dir)
@@ -44,7 +45,7 @@ archive_write_dir <- function(archive, dir, ..., recursive = TRUE, full.names = 
   if (windows_archive(archive)) {
     zip(archive, files)
   } else {
-    (fix_tar(tar))(archive, files)
+    (fix_tar(tar))(archive, files, tar = "internal")
   }
   invisible()
 }

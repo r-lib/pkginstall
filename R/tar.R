85bf5b05f23f9d672ca8fc64c8212631604e93ef
Gábor Csárdi <csardi.gabor@gmail.com>
2018-02-26 00:11:04 +0000

    untar background processes

---
+++ R/tar.R
@@ -0,0 +1,184 @@
+
+#' Create a tar background process
+#'
+#' Use an external tar program, if there is a working one, otherwise use
+#' the internal implementation.
+#'
+#' When using the internal implementation, we need to start another R
+#' process.
+#'
+#' @param tarfile Tar file.
+#' @param files Files or regular expressions to set what to extract. if
+#'   `NULL` then everything is extracted.
+#' @param exdir Where to extract the archive. It must exist.
+#' @param restore_times Whether to restore file modification times.
+#' @return The [callr::process] object.
+#' @keywords internal
+
+make_untar_process <- function(tarfile, files = NULL, exdir = ".",
+                               restore_times = TRUE) {
+  internal <- need_internal_tar()
+  if (internal) {
+    r_untar_process$new(tarfile, files, exdir, restore_times)
+  } else {
+    external_untar_process$new(tarfile, files, exdir, restore_times)
+  }
+}
+
+#' Check if we need to use R's internal tar implementation
+#'
+#' This is slow, because we need to start an R child process, and the
+#' implementation is also very slow. So it is better to use an extranl tar
+#' program, if we can. We test this by trying to uncompress a .tar.gz
+#' archive using the external program. The name of the tar program is
+#' taken from the `TAR` environment variable, if this is unset then `tar`
+#' is used.
+#'
+#' @return Whether we need to use the internal tar implementation.
+#' @keywords internal
+
+need_internal_tar <- (function() {
+  internal <- NULL
+  function() {
+    if (!is.null(internal)) return(internal)
+    mkdirp(tmp <- tempfile())
+    on.exit(unlink(tmp, recursive = TRUE), add = TRUE)
+    tarfile <- system.file(package = .packageName, "tools", "pkg_1.0.0.tgz")
+    p <- external_untar_process$new(tarfile, exdir = tmp)
+    p$wait(timeout = 2000)
+    p$kill()
+    internal <<- p$get_exit_status() != 0 ||
+      !file.exists(file.path(tmp, "pkg", "DESCRIPTION"))
+    internal
+  }
+})()
+
+#' @importFrom R6 R6Class
+
+external_untar_process <- R6Class(
+  "external_untar_process",
+  inherit = callr::process,
+
+  public = list(
+    initialize = function(tarfile, files = NULL, exdir = ".",
+                          restore_times = TRUE,
+                          tar = Sys.getenv("TAR", "tar"))
+      eup_init(self, private, super, tarfile, files, exdir,
+               restore_times, tar)
+  ),
+
+  private = list(
+    options = NULL
+  )
+)
+
+r_untar_process <- R6Class(
+  "r_untar_process",
+  inherit = callr::r_process,
+
+  public = list(
+    initialize = function(tarfile, files = NULL, exdir = ".",
+                          restore_times = TRUE)
+      runtar_init(self, private, super, tarfile, files, exdir,
+                  restore_times, tar)
+  ),
+
+  private = list(
+    options = NULL
+  )
+)
+
+eup_init <- function(self, private, super, tarfile, files, exdir,
+                     restore_times, tar) {
+
+  private$options <- list(
+    tarfile = normalizePath(tarfile),
+    files = files,
+    exdir = exdir,
+    restore_times = restore_times,
+    tar = tar)
+
+  private$options$args <- eup_get_args(private$options)
+  super$initialize(tar, private$options$args)
+  invisible(self)
+}
+
+eup_get_args <- function(options) {
+  c(
+    "-x", "-f", options$tarfile,
+    "-C", options$exdir,
+    get_untar_decompress_arg(options$tarfile),
+    if (! options$restore_times) "-m",
+    options$files
+  )
+}
+
+get_untar_decompress_arg <- function(tarfile) {
+  if (is_gzip_file(tarfile)) {
+    "-z"
+  } else if (is_bzip2_file(tarfile)) {
+    "-j"
+  } else if (is_xz_file(tarfile)) {
+    "-J"
+  } else {
+    character()
+  }
+}
+
+is_gzip_file <- function(file) {
+  buf <- readBin(file, what = "raw", n = 3)
+  length(buf) == 3 &&
+    buf[1] == 0x1f &&
+    buf[2] == 0x8b &&
+    buf[3] == 0x08
+}
+
+is_bzip2_file <- function(file) {
+  buf <- readBin(file, what = "raw", n = 3)
+  length(buf) == 3 &&
+    buf[1] == 0x42 &&
+    buf[2] == 0x5a &&
+    buf[3] == 0x68
+}
+
+is_xz_file <- function(file) {
+  buf <- readBin(file, what = "raw", n = 6)
+  length(buf) == 6 &&
+    buf[1] == 0xFD &&
+    buf[2] == 0x37 &&
+    buf[3] == 0x7A &&
+    buf[4] == 0x58 &&
+    buf[5] == 0x5A &&
+    buf[6] == 0x00
+}
+
+#' @importFrom callr r_process_options
+
+runtar_init <- function(self, private, super, tarfile, files, exdir,
+                        restore_times, tar) {
+
+  options <- list(
+    tarfile = normalizePath(tarfile),
+    files = files,
+    exdir = exdir,
+    restore_times = restore_times,
+    tar = tar,
+    post_process = post_process)
+
+  process_options <- r_process_options()
+  process_options$func <- function(options) {
+    utils::untar(
+      tarfile = options$tarfile,
+      files = options$files,
+      list = FALSE,
+      exdir = options$exdir,
+      compressed = NA,
+      restore_times = options$restore_times,
+      tar = "internal"
+    )
+
+    if (!is.null(options$post_process)) options$post_process() else ret
+  }
+  process_options$args <- list(options = options)
+  super$initialize(process_options)
+}
